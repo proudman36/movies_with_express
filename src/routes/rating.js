1cd14a5821697890e2ef722a44d510efd144fd92
Sebastian sarmiento <sebasstiansarmi77@gmail.com>
2023-02-23 12:07:05 -0500

    se agrego put y delete en rating y reviewer

--- src/routes/rating.js
+++ src/routes/rating.js
@@ -21,13 +21,30 @@ router.post(`/${path}`, (req,res) => {
      .catch((error) => res.json({message: error}) )
  })
 
- router.get(`/${path}`, (req,res) =>{
+ router.get(`/${path}:id`, (req,res) =>{
     const { id } = req.params
     ratingSchema
     .findById(id)
      .then((data) => res.json(data))
      .catch((error) => res.json({message: error}) )
  })
+ router.delete(`/${path}:id`, (req, res) => {
+    const { id } = req.params;
+    userSchema
+      .remove({ _id: id })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+  
+  router.put(`/${path}:id`, (req, res) => {
+    const { id } = req.params;
+    const {mov_id, rev_id, rev_stars, num_o_ratings} = req.body;
+    userSchema
+      .updateOne({ _id: id }, { $set: { mov_id, rev_id, rev_stars, num_o_ratings}})
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
 
  
 
